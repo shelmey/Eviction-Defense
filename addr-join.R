3fc0b3e82bdd7485a4e34dda89cc70af8188dc04
SAM-PC\Sam <samhelmey55@gmail.com>
2019-02-15 09:07:56 -0500

    Update addr-join.R

--- addr-join.R
+++ addr-join.R
@@ -1,11 +1,26 @@
 library(sf)
-
+library(zoo)
 baci.in <- "C:/Bmore City/Shapefiles/BACI Parcels/BACI.shp"
 
 baci <- read_sf(baci.in)
 
 st.types <- read.csv("C:/utilities/street types.csv", strip.white = T, stringsAsFactors = F)
+st.types<- within(st.types,{
+  Primary.Street.Suffix.Name <- ifelse(Primary.Street.Suffix.Name=="",NA,Primary.Street.Suffix.Name)
+  Commonly.Used.Street.Suffix.or.Abbreviation<- ifelse(Commonly.Used.Street.Suffix.or.Abbreviation=="",NA,Commonly.Used.Street.Suffix.or.Abbreviation)
+  Primary.Street.Suffix.Name <- na.locf(Primary.Street.Suffix.Name)
+  Commonly.Used.Street.Suffix.or.Abbreviation <- na.locf(Commonly.Used.Street.Suffix.or.Abbreviation)
+  Postal.Service.Standard.Suffix.Abbreviation<- ifelse(Postal.Service.Standard.Suffix.Abbreviation=="",NA,Postal.Service.Standard.Suffix.Abbreviation)
+  Postal.Service.Standard.Suffix.Abbreviation <- na.locf(Postal.Service.Standard.Suffix.Abbreviation)
+})
 
+sts<-unique(c(st.types$Postal.Service.Standard.Suffix.Abbreviation,st.types$Commonly.Used.Street.Suffix.or.Abbreviation, st.types$Primary.Street.Suffix.Name))
+streets<-unique(rbind(select(st.types,standard=Postal.Service.Standard.Suffix.Abbreviation,actual=Primary.Street.Suffix.Name),
+select(st.types,standard=Postal.Service.Standard.Suffix.Abbreviation,actual=Commonly.Used.Street.Suffix.or.Abbreviation)))
+same<-cbind.data.frame(unique(streets$standard),unique(streets$standard))
+names(same) <- c("standard","actual")
+streets <- unique(rbind(streets,same))
+streets<-filter(streets,!(standard=="MDWS" & actual=="MDW"))
 address.clean <- function(x){
   
   y <- gsub("\\.","",toupper(x))
@@ -14,7 +29,7 @@ address.clean <- function(x){
 which.type <- function(type){
   grepl(paste0(" ",type," .*$"),z)
 }
-sts<-unique(c(st.types$Postal.Service.Standard.Suffix.Abbreviation,st.types$Commonly.Used.Street.Suffix.or.Abbreviation, st.types$Primary.Street.Suffix.Name))
+
   type<-sts[which(unlist(lapply(sts,which.type)))]
   z<- sub(paste0(" ",type," .*$"),paste0(" ",type),z)  
    # lapply(st.types$Postal.Service.Standard.Suffix.Abbreviation, trunc.ad)
@@ -31,3 +46,6 @@ z <- ifelse(grepl(" DR .* DR ",z),
 
 evictions.cleaning.3$ADDRESS <- unlist(lapply(evictions.cleaning.3$ADDRESS,address.clean))
 # baci$ADDRESS <- unlist(lapply(baci$ADDRESS,address.clean))
+evictions.cleaning.3$end<-gsub("^.* ","",evictions.cleaning.3$ADDRESS)
+ev.ad.clean <- left_join(evictions.cleaning.3,dplyr::rename(streets,end=actual))
+
